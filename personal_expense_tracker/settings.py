873683b4053daba2300aa303a7126d07bc29975d
Afrid <Afrid4499@gmail.com>
2025-08-12 13:07:03 +0530

    Prepare project for Heroku deployment

--- personal_expense_tracker/settings.py
+++ personal_expense_tracker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3=byhwtvc43zy401ek=jsv0u%#7-j3soz97ir_t!)m$dr*q4(a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -96,6 +96,16 @@ WSGI_APPLICATION = 'personal_expense_tracker.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
+
+
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'expenses_db',
@@ -104,7 +114,7 @@ DATABASES = {
         'HOST':'localhost',
         'PORT':'5432',
     }
-}
+}"""
 
 
 # Password validation
@@ -141,10 +151,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+"""STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'tracker/static')]"""
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'tracker/static')]
+
+import django_heroku
+
+django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
